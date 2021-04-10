aba30910ae4b188b156cf8837c4e074de87243a6
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-04-11 02:31:52 +0300

    Add register, login to front end. Also profile page to view and update your infos.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Footer from './Components/Footer';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
 import CartScreen from './Screens/CartScreen';
+import LoginScreen from './Screens/LoginScreen';
+import RegisterScreen from './Screens/RegisterScreen';
+import ProfileScreen from './Screens/ProfileScreen';
 
 const App = () => {
   return (
@@ -13,9 +16,12 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/profile" component={ProfileScreen} />
+          <Route path="/register" component={RegisterScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
